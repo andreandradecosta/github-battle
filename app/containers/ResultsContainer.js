d3bc7bb8b845985eb1cb65b01e0152808880db1a
Andre Costa <andre.andrade.costa@gmail.com>
2016-10-07 14:18:11 -0300

    Classes.

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Results from '../components/Results';
 import { battle } from '../utils/githubHelpers';
 
-const ResultsContainer = React.createClass({
-  getInitialState() {
-    return {
-      isLoading: true,
-      scores: []
-    }
-  },
+class ResultsContainer extends Component {
+
+  constructor() {
+      super()
+      this.state =  {
+          isLoading: true,
+          scores: []
+      }
+  }
+
   async componentDidMount() {
     try {
         const scores = await battle(this.props.location.state.playersInfo)
@@ -19,7 +22,8 @@ const ResultsContainer = React.createClass({
     } catch(error) {
         console.warn('Error in ResultsContainer:', error);
     }
-  },
+  }
+
   render() {
     return (
       <Results
@@ -28,6 +32,6 @@ const ResultsContainer = React.createClass({
         scores={this.state.scores} />
     )
   }
-});
+}
 
 export default ResultsContainer;
